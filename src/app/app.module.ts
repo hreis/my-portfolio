f9679dbc88b1c22b77821dfaa82355328b928c7b
Hugo Reis <hugo.carvalho.reis@gmail.com>
2020-03-08 21:51:19 -0300

    Add Skills
    Add skills + introduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { HeroComponent } from './hero/hero.component';
 import { IntroductionComponent } from './introduction/introduction.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HeroComponent,
-    IntroductionComponent
+    IntroductionComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
