c6fbe821ca7ca183e3c74b0bed929512cab582d1
Hugo Reis <hugo.carvalho.reis@gmail.com>
2020-03-12 14:48:32 -0300

    Starting startups projects

---
+++ src/app/startups/startups.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-startups',
+  templateUrl: './startups.component.html',
+  styleUrls: ['./startups.component.scss']
+})
+export class StartupsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
