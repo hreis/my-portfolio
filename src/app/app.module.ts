9be928640cd8892e48d0edf6de7c0d31e73ac588
Hugo Reis <hugo.carvalho.reis@gmail.com>
2020-04-06 17:14:20 -0300

    added responsive + added dev button on startups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
@@ -12,18 +11,15 @@ import { HeroComponent } from './hero/hero.component';
 import { SkillsComponent } from './skills/skills.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CompaniesComponent } from './companies/companies.component';
-import { MyProjectsComponent } from './my-projects/my-projects.component';
 import { StartupsComponent } from './startups/startups.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     HeroComponent,
     SkillsComponent,
     CompaniesComponent,
-    MyProjectsComponent,
     StartupsComponent,
     TestimonialsComponent
   ],
